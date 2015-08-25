b922dab99146f5d69945ba1f52f25dbff079775e
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-25 10:31:49 -0700

    Started writing columns

---
+++ js/block.js
@@ -0,0 +1,8 @@
+var Block = function ($canvas, color) {
+  this.$canvas = $canvas;
+  this.color = color;
+  this.x = 100;
+  this.y = 7;
+};
+
+Block.prototype.d
---
+++ js/game.js
@@ -0,0 +1,11 @@
+$(function () {
+  var $c = $("canvas");
+
+  $c.drawRect({
+    fillStyle: "#000",
+    x: 100,
+    y: 7,
+    width: 25,
+    height: 10
+  });
+});
