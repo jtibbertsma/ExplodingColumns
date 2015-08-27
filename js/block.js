7851ca21d93a2bc81e926b1d8ea3983022b1dfbf
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-27 12:32:29 -0700

    Can move pairs left and right

--- js/block.js
+++ js/block.js
@@ -22,44 +22,6 @@ $(function () {
     this.view.canvas.add(this.rect);
   };
 
-  // actions
-  // 1: move left
-  // 2: move right
-  // 3: drop
-  // 4: rotate
-
-  Block.prototype.doAction = function () {
-    var action = this.pendingAction;
-    this.pendingAction = null;
-
-    switch (action) {
-      case 1:
-        this.moveAndContinueFalling(-1);
-        break
-      case 2:
-        this.moveAndContinueFalling(1);
-        break;
-    }
-  };
-
-  Block.prototype.checkForPendingActions = function () {
-    if (this.view.keyPresses.a && this.canMoveLeft()) {
-      this.view.keyPresses.a -= 1;
-      this.pendingAction = 1;
-
-      return true;
-    }
-
-    if (this.view.keyPresses.d && this.canMoveRight()) {
-      this.view.keyPresses.d -= 1;
-      this.pendingAction = 2;
-
-      return true;
-    }
-
-    return false;
-  };
-
   Block.prototype.canMoveLeft = function () {
     var _canMove = this.canMove(-1);
     if (!_canMove) {
@@ -91,7 +53,7 @@ $(function () {
     return bottomHeight < colHeight;
   };
 
-  Block.prototype.moveAndContinueFalling = function (dir) {
+  Block.prototype.move = function (dir) {
     this.col += dir;
     this.rect.set({left: this.col * 20});
     this.fallTo = this.calculateFallTo();
--- js/pair.js
+++ js/pair.js
@@ -22,12 +22,57 @@ $(function () {
       col: options.startCol,
       fallSpeed: this.fallSpeed
     });
+
+    this.topBlock = this.primaryBlock;
+    this.bottomBlock = this.secondaryBlock;
+    this.leftBlock = null;
+    this.rightBlock = null;
   };
 
   Pair.prototype.timeToStop = function () {
-    if (this.secondaryBlock.timeToStop()) {
-      this.secondaryBlock.stop();
-      this.primaryBlock.stop();
+    if (this.bottomBlock.timeToStop()) {
+      this.bottomBlock.stop();
+      this.topBlock.stop();
+      return true;
+    }
+
+    return false;
+  };
+
+  // actions
+  // 1: move left
+  // 2: move right
+  // 3: drop
+  // 4: rotate
+
+  Pair.prototype.doAction = function () {
+    var action = this.pendingAction;
+    this.pendingAction = null;
+
+    switch (action) {
+      case 1:
+        this.primaryBlock.move(-1);
+        this.secondaryBlock.move(-1);
+        break
+      case 2:
+        this.primaryBlock.move(1);
+        this.secondaryBlock.move(1);
+        break;
+    }
+  };
+
+  Pair.prototype.checkForPendingActions = function () {
+    if (this.view.keyPresses.a && this.bottomBlock.canMoveLeft()) {
+      this.view.keyPresses.a -= 1;
+      this.pendingAction = 1;
+
+      return true;
+    }
+
+    if (this.view.keyPresses.d && this.bottomBlock.canMoveRight()) {
+      this.view.keyPresses.d -= 1;
+      this.pendingAction = 2;
+
       return true;
     }
 
@@ -36,9 +81,9 @@ $(function () {
 
   Pair.prototype.startFalling = function () {
     this._interval = setInterval(function () {
-      // if (this.checkForPendingActions()) {
-      //   this.doAction();
-      // }
+      if (this.checkForPendingActions()) {
+        this.doAction();
+      }
       if (this.timeToStop()) {
         clearInterval(this._interval);
         this.view.canvas.fire("nextIteration");
