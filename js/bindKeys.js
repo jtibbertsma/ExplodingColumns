5bb0264c62e0351eb1864a982dde22663637cf99
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-12 16:16:48 -0700

    Don't carelessly prevent default for every keydown event

--- js/bindKeys.js
+++ js/bindKeys.js
@@ -5,27 +5,31 @@ $(function () {
 
   Columns.bindKeys = function (keyPresses) {
     $(document).on("keydown", function (event) {
-      event.preventDefault();
       var code = event.keyCode;
 
       switch (code) {
         case 65:
         case 37:
+          event.preventDefault();
           keyPresses.a += 1;
           break;
         case 83:
         case 40:
+          event.preventDefault();
           keyPresses.s += 1;
           break;
         case 68:
         case 39:
+          event.preventDefault();
           keyPresses.d += 1;
           break;
         case 87:
         case 38:
+          event.preventDefault();
           keyPresses.w += 1;
           break;
         case 80:
+          event.preventDefault();
           keyPresses.p += 1;
           break;
       }
